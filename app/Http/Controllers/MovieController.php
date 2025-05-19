2b1d843622aa6622a18c9bd115a410332269a913
SalsabilaZahraSarwo <salsazhra131@gmail.com>
2025-05-19 16:46:42 +0700

    homepage view

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
     //
+    public function homepage(){
+        //untuk membuat halaman cuma 6 slide
+        $movies = Movie::latest()->paginate(6);
+        return view('homepage', compact('movies'));
+    }
 }
